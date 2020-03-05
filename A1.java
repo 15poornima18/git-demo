534bfe03f4662881700c678ed8d510e984f57122
poornima tomar <tomarpoornima52@gmail.com>
2020-03-05 10:43:44 +0530

    A1 added

---
+++ A1.java
@@ -0,0 +1,12 @@
+class A1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Done");
+    }
+    static void test()
+    {
+        int i;
+        System.out.println(i=0);
+    }
+}
